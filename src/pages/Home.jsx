84e1f491d1310dc997a75e50e98f8b4bc7ac0763
Dafydd <dafydd@Dafydds-MacBook-Air.local>
2023-05-30 13:49:11 +0100

    Basic homepage

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import MainHeading from "../components/textNodes/MainHeading";
+
+function Home ()  {
+    return (
+        <>
+        <MainHeading headingText="Game Review Hub"/>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+        </>
+    )
+}
+
+export default Home;
