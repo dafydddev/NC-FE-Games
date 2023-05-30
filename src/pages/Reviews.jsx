546c8de10ac1b276f0bcc12c945cb6ce6bef4a44
Dafydd <dafydd@Dafydds-MacBook-Air.local>
2023-05-30 14:09:46 +0100

    Basic review page

--- src/pages/Reviews.jsx
+++ src/pages/Reviews.jsx
@@ -3,20 +3,26 @@ import reviews from "../api/api";
 
 import MainHeading from "../components/textNodes/MainHeading";
 import ReviewCard from "../components/reviewWidgets/ReviewCard";
+import Loader from "../components/textNodes/Loader";
 
 function Reviews() {
+  const pageName = "Reviews";
   const [reviewData, setReviewData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     reviews.get("/reviews").then(({ data }) => {
       setReviewData(data);
+      setIsLoading(false);
     });
   }, []);
   return (
     <>
-      <MainHeading headingText={"Reviews"} />
-      {reviewData.map((review) => {
-        return <ReviewCard key={review.review_id} review={review} />;
-      })}
+        <MainHeading headingText={pageName} />
+        {isLoading ? <Loader contentName={pageName} /> :
+            reviewData.map((review) => {
+                return <ReviewCard key={review.review_id} review={review} />;
+            })
+        }
     </>
   );
 }
